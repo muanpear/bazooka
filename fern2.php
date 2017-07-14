3e7286d1d7be0e97f91931fbd29abe3be4323a5b
Fern <rew_sea_ken@hotmail.com>
2017-07-14 11:58:08 +0700

    14 commit

--- fern2.php
+++ fern2.php
@@ -1,19 +1,3 @@
 <?php
-$db_config=array(
-    "host"=>"203.151.143.164",
-    "user"=>"root",
-    "pass"=>"noconly",
-    "dbname"=>"line",
-    "charset"=>"utf8"
-);
-$mysqli = @new mysqli($db_config["host"], $db_config["user"], $db_config["pass"], $db_config["dbname"]);
-$mysqli->set_charset($db_config["charset"]);
-if(mysqli_connect_error()) {
-    die('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
-    exit;
-}
 
-$result = $mysqli->query("SELECT * FROM `device_type`");
-$data = $result->fetch_assoc();
-echo $data['deviceTypeName'];
 ?>
