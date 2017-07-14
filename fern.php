c3baef82f07edf97767e6b4381dd4547b7c7ec20
Fern <rew_sea_ken@hotmail.com>
2017-07-14 11:21:22 +0700

    6 commit

--- fern.php
+++ fern.php
@@ -1,7 +1,9 @@
 <?php
 //print_r(file_get_contents("http://172.20.20.225/Json/gen_json1.php"));
 
-$json = file_get_contents('http://203.151.143.172/Json/gen_json1.php');
-$obj = json_decode($json);
-echo $obj->id;
+//$json = file_get_contents('http://203.151.143.172/Json/gen_json1.php');
+$json = file_get_contents('https://www.hotmail.com');
+// $obj = json_decode($json);
+// echo $obj->id;
+print_r($json);
 ?>
