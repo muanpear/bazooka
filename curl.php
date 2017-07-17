63718ae692154b4e8d332b3dbfe2d501bb35cc74
Fern <rew_sea_ken@hotmail.com>
2017-07-17 14:59:40 +0700

    add curl file

---
+++ curl.php
@@ -0,0 +1,12 @@
+<?php
+	$ch1 = curl_init();
+            curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch1, CURLOPT_URL, 'http://203.151.143.172/Json/gen_json1.php');
+            $result1 = curl_exec($ch1);
+            curl_close($ch1);
+            
+            $obj = json_decode($result1, true);
+            // print_r($obj);
+           echo $obj['humidity'];
+?>
