90df81bacb9f136ee7cc2f4a761acd5a3f1c77ed
Fern <rew_sea_ken@hotmail.com>
2017-07-17 16:00:54 +0700

    add test file 10

--- curl.php
+++ curl.php
@@ -1,12 +1,14 @@
 <?php
-	$ch1 = curl_init();
-            curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch1, CURLOPT_URL, 'http://203.151.143.172/Json/file.json');
-            $result1 = curl_exec($ch1);
-            curl_close($ch1);
+	// $ch1 = curl_init();
+ //            curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
+ //            curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
+ //            curl_setopt($ch1, CURLOPT_URL, 'http://www.google.co.th');
+ //            $result1 = curl_exec($ch1);
+ //            curl_close($ch1);
             
-            $obj = json_decode($result1, true);
-            // print_r($obj);
-           echo $obj['humidity'];
+ //            $obj = json_decode($result1, true);
+ //            print_r($obj);
+           // echo $obj['humidity'];
+          $json = file_get_contents('http://203.151.143.173:8080/app/index.php');
+print_r($json);
 ?>
