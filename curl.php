5f9404903cc87d57ceb2fb180f1961003f860896
Fern <rew_sea_ken@hotmail.com>
2017-07-18 09:23:09 +0700

    wow2

--- curl.php
+++ curl.php
@@ -7,18 +7,19 @@
 
 
 <?php
-	// $ch1 = curl_init();
- //            curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
- //            curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
- //            curl_setopt($ch1, CURLOPT_URL, 'http://www.google.co.th');
- //            $result1 = curl_exec($ch1);
- //            curl_close($ch1);
+	$ch1 = curl_init();
+            curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch1, CURLOPT_URL, 'http://203.151.143.172/Json/gen_json1.php');
+            $result1 = curl_exec($ch1);
+            curl_close($ch1);
             
- //            $obj = json_decode($result1, true);
- //            print_r($obj);
+           //  $obj = json_decode($result1, true);
+           //  print_r($obj);
            // echo $obj['humidity'];
-          $json = file_get_contents('http://203.151.143.172/Json/gen_json1.php');
-print_r($json);
+           print_r($result1);
+//           $json = file_get_contents('http://203.151.143.172/Json/gen_json1.php');
+// print_r($json);
 ?>
 	
 </body>
